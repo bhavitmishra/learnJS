02f120a761d9b6ae0770bea6372884c368953b0c
Bhavit Mishra <139310721+Mr-ZordiaK@users.noreply.github.com>
2024-12-25 03:49:32 +0000

    operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,7 +1,28 @@
 let score = 33;
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueInString = String(score);
-console.log(typeof valueInString);
-console.log(valueInString);
+// console.log(typeof valueInString);
+// console.log(valueInString);
+
+
+
+// *********************** Operations ****************************//
+
+let value = 3;
+let negvalue = -value;
+
+// console.log(negvalue);
+
+let str1 = "hello";
+let str2 = " bhavit";
+
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+
