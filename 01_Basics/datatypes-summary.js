744927f3fadba657e46d7f0290e154eb97ad02a5
Bhavit Mishra <139310721+Mr-ZordiaK@users.noreply.github.com>
2024-12-25 04:51:46 +0000

    datatype-summary

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// # primitive 
+
+
+// 7 types : String , Number , Boolean , Null , Undefined , Symbol , BigInt
+
+
+
+// ye sb pdh chuke hai ---^
+
+// Reference (non primitive)
+
+// array , objects , functions
+
+
+// Arrays :
+
+const heroes = ["Optimus" , "PIkachu" , "FourArms"];
+
+
+// objects
+
+let myObj = {
+    name: "Bhavit" , 
+    age: 21
+}
+
+const myFunction = function(){
+    console.log("This is My Function");
+    
+}
+
