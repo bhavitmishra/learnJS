ce0329b4216b98815d13243901b3f6ffe8f03382
Bhavit Mishra <139310721+Mr-ZordiaK@users.noreply.github.com>
2024-12-25 07:38:46 +0000

    date

---
+++ 01_Basics/07_Dates_in_JS.js
@@ -0,0 +1,16 @@
+//Dates
+
+let myDate = new Date();
+
+// console.log(myDate.toISOString());
+// console.log(myDate.toDateString());
+
+// let myCreatedDate = new Date(2024 , 11 , 25);
+let myCreatedDate = new Date(2024 , 11 , 25 , 1 , 2);
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp);
+
+
