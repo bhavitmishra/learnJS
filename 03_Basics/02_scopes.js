69a803084cc611555f8233da9224639d8e5866c9
Bhavit Mishra <139310721+bhavitmishra@users.noreply.github.com>
2024-12-25 14:14:29 +0000

    scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,33 @@
+let a = 300;
+if(true)
+{
+    let a = 69;
+    // console.log("Inner a = " , a);
+}
+
+// console.log("Outer a = " , a);
+
+
+function one() {
+
+    const username = "Bhavit";
+
+    function two(){
+        const website = "mishras";
+        console.log(username);
+        console.log(website);
+        
+    }
+    two();
+}
+// one();
+
+// ++++++++++++++++++  interesting +++++++++++++++++++++++
+
+addone(5);
+function addone(num) {return num+1;}
+
+addtwo(5);
+const addtwo = function(num){return num+2;}
+
+
