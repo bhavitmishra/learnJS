945751d11b4d99f86ce8f6ffa308280c5b5dd48b
Bhavit Mishra <139310721+Mr-ZordiaK@users.noreply.github.com>
2024-12-25 05:27:20 +0000

    strings

---
+++ 01_Basics/05_string.js
@@ -0,0 +1,37 @@
+const name = "Bhavit"
+const repoCount = 50;
+
+//Outdated 
+// {
+// console.log(name + repoCount + "Value");
+//}
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+let val = "dingding";
+//console.log(val.length);
+
+const gameName = new String('Bhavit');
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.toUpperCase());
+
+const newgname = gameName.slice(-2 , 5);
+// console.log(newgname);
+
+const url = "https://bhavitmishra%20.com";
+
+
+console.log(url.replace("%20" , ""));
+console.log(url);
+
+const arr = url.split('%20');
+console.log(arr);
+
+
+
+
+
+
+
