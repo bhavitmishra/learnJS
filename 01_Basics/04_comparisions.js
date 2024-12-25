5a231d091f691c2e9882e4e5e2d853a072c159af
Bhavit Mishra <139310721+Mr-ZordiaK@users.noreply.github.com>
2024-12-25 04:35:45 +0000

    comparisions

---
+++ 01_Basics/04_comparisions.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// ===  strict check(datatype bhi dekhega)
+
+
+
+/* ***********************************same datatype se compare karo aur upar waali bkchoodi se bachoo ***************************************** */
