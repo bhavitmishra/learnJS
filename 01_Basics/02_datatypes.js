3249fd4e963e5abb6b61bd98c49a892772d4b158
Bhavit Mishra <139310721+Mr-ZordiaK@users.noreply.github.com>
2024-12-25 03:30:26 +0000

    datatypes

--- 01_Basics/02_datatypes.js
+++ 01_Basics/02_datatypes.js
@@ -2,4 +2,20 @@
 
 // alert(3 + 3) // we are using nodejs , not browser
 
-// avoid
+let name = "mishra"
+let age = 21
+let isLoggedIn = false
+
+
+//number => 2^53
+//bigint
+//string => ""
+//boolean => T/F
+//null => standalone value
+//undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof undefined)
+console.log(typeof null)
