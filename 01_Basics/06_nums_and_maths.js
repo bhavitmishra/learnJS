5bd70f0125f2d029b143ad3b9f5f8b011772787f
Bhavit Mishra <139310721+Mr-ZordiaK@users.noreply.github.com>
2024-12-25 07:15:39 +0000

    random fn

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,11 @@
+// kuch ni full bkchodi hai
+
+
+
+// +++++++++++++++++++++++   MATHS    +++++++++++++++++++++++++
+
+console.log((Math.floor(Math.random()*6) + 1));
+
+// range me lane k liye
+
+// multiply with max-min+1 and then add min value outside the braces to get the result in range
