3c1648505da44b00745eaa0803cedc4a1c0b8055
Bhavit Mishra <139310721+bhavitmishra@users.noreply.github.com>
2024-12-26 07:13:06 +0000

    control statements done

---
+++ 04_Control-Flow/one.js
@@ -0,0 +1,53 @@
+// if
+
+// Aata hai yrr
+
+
+//switch case statement
+
+const month= 69;
+
+switch(month){
+    case 1:
+        console.log("January");
+        break
+    case 2:
+        console.log("Feb");
+        break
+    case 3:
+        console.log("MAr");
+        break
+    case 4:
+        console.log("aPR");
+        break
+    case 5:
+        console.log("MAY");
+        break
+    case 6:
+        console.log("JUNE");
+        break
+    case 7:
+        console.log("JULY");
+        break
+    case 8:
+        console.log("AUG");
+        break
+    case 9:
+        console.log("SEPT");
+        break
+    case 10:
+        console.log("OCTT");
+        break
+    case 11:
+        console.log("NOV");
+        break
+    case 12:
+        console.log("DEC");
+        break
+    default:
+        console.log("no matched");
+}
+
+
+
+
---
+++ 04_Control-Flow/truthy.js
@@ -0,0 +1,32 @@
+// const userEmail = "bhavit@ai";
+
+// if(userEmail)
+// {
+//     console.log("Got the Email");
+    
+// }
+// else console.log("don't have email");
+
+
+/**
+ flasy values : flase , 0 , -1 , "" , null , undefined , NaN
+ baaki saare truthy values
+ */
+
+
+
+ // Nullish coalescing operator (??) : null / undefined
+
+ let val1;
+ val1 = null ?? undefined;
+
+//  console.log(val1);
+
+
+//ternary operator
+
+const iceTeaPrice = 100;
+
+iceTeaPrice > 80 ? console.log("expensive") :console.log( "nice, cheap");
+
+
