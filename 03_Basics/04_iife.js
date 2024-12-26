393f4785f0d94d1efb9cefdf89d8d9eeb03c3908
Bhavit Mishra <139310721+bhavitmishra@users.noreply.github.com>
2024-12-26 07:12:51 +0000

    iife & basics completed

--- 03_Basics/04_iife.js
+++ 03_Basics/04_iife.js
@@ -1,9 +1,17 @@
 // Immediately Invoked Function Expressions (IIFE)
 
-(function chai() {
-    console.log(`DB Connected`)
-})();
+// (function chai() {
+//     console.log(`DB Connected`)
+// })();
 
-( (name) => {
-    console.log(`DB two CONNECTED ${name}`);
-})("Bhavit");
+// ( (name) => {
+//     console.log(`DB two CONNECTED ${name}`);
+// })("Bhavit");
+
+
+
+
+(()=> (console.log(this)))(); 
+
+
+//Basics Khatam Hogya haiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
